6ac2b0de7125b71578ccdd1ac165801f641ae132
Maks <maksymbornak@gmail.com>
2022-11-30 16:47:08 +0200

    'lab'

---
+++ lab 5/lab 5/lab5.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+class TimeConventer {
+	int hours = 5;
+	int minutes = 37;
+	int seconds = 42;
+public:
+	void outputConvertedTime() {
+		std::cout << hours  << minutes << seconds << std::endl;
+	}
+};
