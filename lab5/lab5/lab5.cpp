9aec41bae303a8836cca0d51545107345467d424
Maks <maksymbornak@gmail.com>
2022-12-21 17:22:02 +0200

    Revieved code

--- lab5/lab5/lab5.cpp
+++ lab5/lab5/lab5.cpp
@@ -1,11 +1,41 @@
 ﻿#include <iostream>
 
-class TimeConventer {
-	int hours = 5;
-	int minutes = 37;
-	int seconds = 42;
+class TimeConverter {
+private:
+    double hours_;
+    int minutes_;
+    int seconds_;
 public:
-	void outputConvertedTime() {
-		std::cout << hours  << minutes << seconds << std::endl;
-	}
+    int outputConvertedTime(double seconds_to_transfer_) {
+        hours_ = seconds_to_transfer_ / 3600;
+        std::cout << hours_ << " hours" << std::endl;
+        return 0;
+    }
+    int outputConvertedTime(int hours_to_transfer_) {
+        seconds_ = hours_to_transfer_ * 3600;
+        std::cout << seconds_ << " seconds" << std::endl;
+        return 0;
+    }
+    int outputConvertedTime(float hours_, int minutes_, int seconds_) {
+        std::cout << hours_ << ":" << minutes_ << ":" << seconds_ << std::endl;
+        seconds_ += ((hours_ * 60) + minutes_) * 60;
+        std::cout << seconds_ << " seconds" << std::endl;
+        hours_ = seconds_ / 3600;
+        if (minutes_ > 30) {
+            std::cout << hours_ << ",5 hours" << std::endl;
+        }
+        else {
+            std::cout << hours_ << " hours" << std::endl;
+        }
+    }
+
 };
+int main() {
+    TimeConverter transfer;
+    transfer.outputConvertedTime(12, 29, 42);
+    transfer.outputConvertedTime(2);
+    transfer.outputConvertedTime(27);
+
+
+    return 0;
+}
